f388d507117d349f0f95110d2a12a8e4bacda84e
Swashy OSX <roland.macdavid@gmail.com>
2017-03-10 19:53:01 -0500

    enable iptables

--- debian-nmap.sh
+++ debian-nmap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#<UDF name="pubkey" Label="Enter your public key here" default=""/>
+#
+#<UDF name="pubkey" Label="Enter your public key here" default="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5c8Eyp/+gET8irVN6ck2/eC7jcAaPF7bKJBWbe4w8Df61jwaaBHREH33R65cxOZtC0FRwvOU3uDMyGh+Zqt1Pwab15hmFs98LLZ3ZwPC6GPhIZlAUD78l8ZHV2tW2N4XWBU65Ek3SDOiDg/YHswg2S6lwQ8GlwloNlt9oaydXsZwReJfMqQO6JSj8QN0YdNoeGfC3cipx8H3k3p45dJDtssXu+qlC/lLkpLMuChGG+mMuIGN45Emrb0kEqAfQeGjb5HVN6kg8r0OQi/2YWEauSkFTIy5ghBScEf2C/aveagZASFSdjb5bFT+D/Gm+8IcNYkd5RZaYuxWyK+fExllb"/>
 #
 apt-get -o Acquire::ForceIPv4=true update -y
 apt-get -o Acquire::ForceIPv4=true install ethtool nmap -y
@@ -14,3 +15,10 @@ iptables --table raw  -I PREROUTING -m state --state INVALID -j ACCEPT
 iptables --table nat  -I OUTPUT -m state --state INVALID -j ACCEPT
 iptables --table nat  -I POSTROUTING -m state --state INVALID -j ACCEPT
 yes | sudo apt-get purge iptables-persistent -y
+
+mkdir -p /root/.ssh/
+touch /root/.ssh/authorized_keys
+echo "$publickey" > /root/.ssh/authorized_keys
+
+sed -i.bak "/PasswordAuthentication/ s/yes/no/" /etc/ssh/sshd_config
+systemctl restart sshd
