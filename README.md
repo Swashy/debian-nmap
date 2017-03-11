5c1005579284f07f2ddbbaf5175f0534e50cefa4
Swashy OSX <roland.macdavid@gmail.com>
2017-03-11 15:46:13 -0500

    pubkey UDF var now works

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 https://www.linode.com/stackscripts/view/74940
+
+This Linode stackscript will make a basic Debian install with some tweaks to the network interface and iptables to make nmap scanning work.
+
+Note that this is not something you really want to use because it breaks iptables built-in filters for invalid packages. That's probably a security risk.
--- debian-nmap.sh
+++ debian-nmap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 #<UDF name="pubkey" Label="Enter your public key here" default="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5c8Eyp/+gET8irVN6ck2/eC7jcAaPF7bKJBWbe4w8Df61jwaaBHREH33R65cxOZtC0FRwvOU3uDMyGh+Zqt1Pwab15hmFs98LLZ3ZwPC6GPhIZlAUD78l8ZHV2tW2N4XWBU65Ek3SDOiDg/YHswg2S6lwQ8GlwloNlt9oaydXsZwReJfMqQO6JSj8QN0YdNoeGfC3cipx8H3k3p45dJDtssXu+qlC/lLkpLMuChGG+mMuIGN45Emrb0kEqAfQeGjb5HVN6kg8r0OQi/2YWEauSkFTIy5ghBScEf2C/aveagZASFSdjb5bFT+D/Gm+8IcNYkd5RZaYuxWyK+fExllb"/>
-#
+#PUBKEY=
+
 apt-get -o Acquire::ForceIPv4=true update -y
 apt-get -o Acquire::ForceIPv4=true install ethtool nmap -y
 
@@ -14,12 +15,16 @@ iptables -I OUTPUT -m state --state INVALID -j ACCEPT
 iptables --table raw  -I PREROUTING -m state --state INVALID -j ACCEPT
 iptables --table nat  -I OUTPUT -m state --state INVALID -j ACCEPT
 iptables --table nat  -I POSTROUTING -m state --state INVALID -j ACCEPT
-yes | sudo apt-get install iptables-persistent -y
+
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+apt-get -o Acquire::ForceIPv4=true install iptables-persistent -y
 systemctl enable iptables-persistent
 
 mkdir -p /root/.ssh/
 touch /root/.ssh/authorized_keys
-echo "$publickey" > /root/.ssh/authorized_keys
+
+echo "$PUBKEY" >> /root/.ssh/authorized_keys
 
 sed -i.bak "/PasswordAuthentication/ s/yes/no/" /etc/ssh/sshd_config
 systemctl restart sshd
